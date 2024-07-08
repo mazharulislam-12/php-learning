8ec66d0c204cfd3443762d1b52bcf22377d49ddd
mazharulislam-12 <505mazharulislam@gmail.com>
2024-07-08 23:16:24 +0600

    loop and array

---
+++ array.php
@@ -0,0 +1,31 @@
+<?php
+// ইনডেক্সড অ্যারে
+$city = array["Dhaka", "Mymenshingh", "Tangil", "Gazipur"];
+
+print_r($city);
+echo "<br>";
+echo $city[1]; 
+echo "<br>";
+
+
+// // অ্যাসোসিয়েটিভ অ্যারে
+    $run = array["Shakib" => 55, "Asif" => 58, "Rakib"=> 45];
+    echo $run["Asif"];
+
+//     // মাল্টিডাইমেনশনাল অ্যারে
+    $student = array (
+        array("Asif", 45),
+        array("Asif", 45),
+        array("Asif", 45),
+    )
+
+    $fruits = array('Apple', 'Mango', 'Orange');
+
+    foreach ($fruits as $key => $fruit) {
+        echo $fruit. "<br>";
+    }
+
+
+
+
+?>
--- loop.php
+++ loop.php
@@ -3,24 +3,34 @@
     // for loop
     // print 1 to 100
 
-//     for($i = 1; $i<=100; $i++){
-//         echo $i."<br>";
-//     }
+    for($i = 1; $i<=100; $i++){
+        echo $i."<br>";
+    }
 
-//    for ($i=0; $i <=100 ; $i +=2) { 
-//     echo $i. "<br>";
-//    }
+   for ($i=0; $i <=100 ; $i +=2) { 
+    echo $i. "<br>";
+   }
 
-//    for ($i=1; $i <100 ; $i++) { 
-//     if ($i % 2 !=0) {
-//         echo $i."<br>";
-//     }
-//    }
+   for ($i=1; $i <100 ; $i++) { 
+    if ($i % 2 !=0) {
+        echo $i."<br>";
+    }
+   }
 
 
 //    while loop
+    $x = 1;
+    while ($x <= 100) {
+        echo $x . '<br>';
+        $x++;
+    }
 
+
+    // do-while loop
+    $y = 1;
+    do {
+        echo $y . "<br>";
+        $y++;
+    } while ($y <= 100);
  
-
-    
 ?>
